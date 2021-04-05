d4d96366de5013312cd80e625ec39f4a970f0124
loki <kiraconan2046@gmail.com>
2021-04-05 13:39:58 +0800

    feat: add InMemoryEventBus

---
+++ dracula-eventbus/src/main/java/com/github/lokic/dracula/eventbus/InMemoryEventBus.java
@@ -0,0 +1,43 @@
+package com.github.lokic.dracula.eventbus;
+
+import com.github.lokic.dracula.event.Event;
+import com.github.lokic.dracula.eventbus.handlers.EventHandler;
+import com.github.lokic.dracula.eventbus.handlers.EventHandlerAttribute;
+import com.github.lokic.dracula.eventbus.interceptors.InterceptorAttribute;
+import com.github.lokic.dracula.eventbus.publihser.InMemoryPublisher;
+import com.github.lokic.dracula.eventbus.publihser.PublisherManagement;
+import com.github.lokic.dracula.eventbus.subscriber.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+public class InMemoryEventBus implements EventBus {
+
+    private final PublisherManagement publisherManagement;
+
+    private final SubscriberManagement subscriberManagement;
+
+    public InMemoryEventBus(PublisherManagement publisherManagement, SubscriberManagement subscriberManagement) {
+        this.publisherManagement = publisherManagement;
+        this.subscriberManagement = subscriberManagement;
+    }
+
+    @Override
+    public <E extends Event> void register(Class<E> eventClazz, EventHandler<E> handler, List<InterceptorAttribute<E>> interceptorAttributes, EventHandlerAttribute eventHandlerAttribute) {
+        Subscriber<E> subscriber = subscriberManagement.addSubscriber(eventClazz,
+                new InMemorySubscriber<>(new SubscriptionImpl<>(eventClazz, handler, interceptorAttributes, eventHandlerAttribute)));
+        publisherManagement.addPublisherIfNotExist(eventClazz, new InMemoryPublisher<>(subscriber));
+    }
+
+    @Override
+    public <E extends Event> void unregister(EventHandler<E> handler) {
+        subscriberManagement.removeSubscriber(handler);
+    }
+
+    @Override
+    public <E extends Event> void post(E event) {
+        publisherManagement.processEvent(event);
+    }
+
+}
