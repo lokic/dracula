4a1913e5632028c065a29e458a868f8819162ebe
loki <kiraconan2046@gmail.com>
2021-04-06 23:20:38 +0800

    feat: add SubscriberManagement addSubscription method

--- dracula-eventbus/src/main/java/com/github/lokic/dracula/eventbus/subscriber/InMemorySubscriber.java
+++ dracula-eventbus/src/main/java/com/github/lokic/dracula/eventbus/subscriber/InMemorySubscriber.java
@@ -3,11 +3,6 @@ package com.github.lokic.dracula.eventbus.subscriber;
 import com.github.lokic.dracula.event.Event;
 
 public class InMemorySubscriber<E extends Event> extends SubscriptionSubscriber<E> {
-
     public InMemorySubscriber() {
-    }
-
-    public InMemorySubscriber(Subscription<E> subscription) {
-        super(subscription);
     }
 }
