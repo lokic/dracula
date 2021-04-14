66c329ba313b59feaaf98986644a967a24ec15be
loki <kiraconan2046@gmail.com>
2021-04-15 01:00:41 +0800

    style: modify style

--- dracula-eventbus-ddd/src/main/java/com/github/lokic/dracula/eventbus/ddd/integration/IntegrationSubscriber.java
+++ dracula-eventbus-ddd/src/main/java/com/github/lokic/dracula/eventbus/ddd/integration/IntegrationSubscriber.java
@@ -4,7 +4,7 @@ import com.github.lokic.dracula.event.Event;
 import com.github.lokic.dracula.eventbus.publisher.ForwardingPublisher;
 import com.github.lokic.dracula.eventbus.subscriber.Subscriber;
 
-public abstract class IntegrationSubscriber<E extends Event> implements Deserializer<E>, Subscriber<E>, ForwardingPublisher<E> {
+public abstract class IntegrationSubscriber<E extends Event> implements Subscriber<E>, ForwardingPublisher<E>, Deserializer<E> {
 
     public void receive(String data) {
         subscribe(deserialize(data));
