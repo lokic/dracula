00bd83daabca9fe59fe169a9c22cfaa22c4d302b
loki <kiraconan2046@gmail.com>
2021-11-11 19:27:13 +0800

    test: 优化单元测试

--- dracula-eventbus/src/test/java/com/github/lokic/dracula/eventbus/interceptor/InterceptorChainImplTest.java
+++ dracula-eventbus/src/test/java/com/github/lokic/dracula/eventbus/interceptor/InterceptorChainImplTest.java
@@ -1,7 +1,7 @@
 package com.github.lokic.dracula.eventbus.interceptor;
 
 import com.github.lokic.dracula.event.Event;
-import com.github.lokic.dracula.eventbus.interceptor.extension.ExtensionInterceptor;
+import com.github.lokic.dracula.eventbus.interceptor.extension.SpyExtensionInterceptor;
 import com.github.lokic.dracula.eventbus.interceptor.internal.SpyInternalInterceptor;
 import org.assertj.core.util.Lists;
 import org.junit.Assert;
@@ -21,9 +21,9 @@ public class InterceptorChainImplTest {
         InterceptorChainImpl<TestEvent> interceptorChain = Mockito.mock(InterceptorChainImpl.class);
         Mockito.doCallRealMethod().when(interceptorChain).parseEnabledInterceptorAttributes(Mockito.anyList(), Mockito.anyList());
 
-        InterceptorAttribute<TestEvent> interceptorAttribute1 = new InterceptorAttribute<>(buildInterceptor("A", 0));
-        InterceptorAttribute<TestEvent> interceptorAttribute2 = new InterceptorAttribute<>(buildInterceptor("B", 1));
-        InterceptorAttribute<TestEvent> interceptorAttribute3 = new InterceptorAttribute<>(buildInterceptor("C", 2));
+        InterceptorAttribute<TestEvent> interceptorAttribute1 = new InterceptorAttribute<>(new SpyExtensionInterceptor<>("A", 0));
+        InterceptorAttribute<TestEvent> interceptorAttribute2 = new InterceptorAttribute<>(new SpyExtensionInterceptor<>("B", 1));
+        InterceptorAttribute<TestEvent> interceptorAttribute3 = new InterceptorAttribute<>(new SpyExtensionInterceptor<>("C", 2));
         InterceptorAttribute<TestEvent> interceptorAttribute4 = new InterceptorAttribute<>(new SpyInternalInterceptor<>("D", 1));
         InterceptorAttribute<TestEvent> interceptorAttribute5 = new InterceptorAttribute<>(new SpyInternalInterceptor<>("E", 2));
         InterceptorAttribute<TestEvent> interceptorAttribute6 = new InterceptorAttribute<>(new SpyInternalInterceptor<>("F", 0));
@@ -80,20 +80,6 @@ public class InterceptorChainImplTest {
                 interceptorChain.parseEnabledInterceptorAttributes(attributes2, Lists.newArrayList(Rule.of("default"), Rule.of("A"), Rule.of("C")))
         );
 
-    }
-
-    public ExtensionInterceptor<TestEvent> buildInterceptor(String name, int order) {
-        return new ExtensionInterceptor<TestEvent>() {
-            @Override
-            public int getOrder() {
-                return order;
-            }
-
-            @Override
-            public String getName() {
-                return name;
-            }
-        };
     }
 
     public static class TestEvent extends Event {
