35609a169d0b765f40b346d30e5e59e0cc30e846
loki <kiraconan2046@gmail.com>
2021-11-11 19:39:35 +0800

    test: 优化单元测试

--- dracula-eventbus/src/test/java/com/github/lokic/dracula/eventbus/interceptor/InterceptorChainImplTest.java
+++ dracula-eventbus/src/test/java/com/github/lokic/dracula/eventbus/interceptor/InterceptorChainImplTest.java
@@ -5,6 +5,7 @@ import com.github.lokic.dracula.eventbus.interceptor.extension.SpyExtensionInter
 import com.github.lokic.dracula.eventbus.interceptor.internal.SpyInternalInterceptor;
 import org.assertj.core.util.Lists;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -14,26 +15,38 @@ import java.util.stream.Stream;
 
 public class InterceptorChainImplTest {
 
+    private InterceptorChainImpl<TestEvent> interceptorChain;
 
-    @Test
-    public void test_parseEnabledInterceptorAttributes() {
-        @SuppressWarnings("unchecked")
-        InterceptorChainImpl<TestEvent> interceptorChain = Mockito.mock(InterceptorChainImpl.class);
+    private List<InterceptorAttribute<? extends TestEvent>> attributes;
+
+    private InterceptorAttribute<TestEvent> interceptorAttribute1;
+    private InterceptorAttribute<TestEvent> interceptorAttribute2;
+    private InterceptorAttribute<TestEvent> interceptorAttribute3;
+    private InterceptorAttribute<TestEvent> interceptorAttribute4;
+    private InterceptorAttribute<TestEvent> interceptorAttribute5;
+    private InterceptorAttribute<TestEvent> interceptorAttribute6;
+
+    @Before
+    public void init() {
+        interceptorChain = Mockito.mock(InterceptorChainImpl.class);
         Mockito.doCallRealMethod().when(interceptorChain).parseEnabledInterceptorAttributes(Mockito.anyList(), Mockito.anyList());
 
-        InterceptorAttribute<TestEvent> interceptorAttribute1 = new InterceptorAttribute<>(new SpyExtensionInterceptor<>("A", 0));
-        InterceptorAttribute<TestEvent> interceptorAttribute2 = new InterceptorAttribute<>(new SpyExtensionInterceptor<>("B", 1));
-        InterceptorAttribute<TestEvent> interceptorAttribute3 = new InterceptorAttribute<>(new SpyExtensionInterceptor<>("C", 2));
-        InterceptorAttribute<TestEvent> interceptorAttribute4 = new InterceptorAttribute<>(new SpyInternalInterceptor<>("D", 1));
-        InterceptorAttribute<TestEvent> interceptorAttribute5 = new InterceptorAttribute<>(new SpyInternalInterceptor<>("E", 2));
-        InterceptorAttribute<TestEvent> interceptorAttribute6 = new InterceptorAttribute<>(new SpyInternalInterceptor<>("F", 0));
+        interceptorAttribute1 = new InterceptorAttribute<>(new SpyExtensionInterceptor<>("A", 0));
+        interceptorAttribute2 = new InterceptorAttribute<>(new SpyExtensionInterceptor<>("B", 1));
+        interceptorAttribute3 = new InterceptorAttribute<>(new SpyExtensionInterceptor<>("C", 2));
+        interceptorAttribute4 = new InterceptorAttribute<>(new SpyInternalInterceptor<>("D", 1));
+        interceptorAttribute5 = new InterceptorAttribute<>(new SpyInternalInterceptor<>("E", 2));
+        interceptorAttribute6 = new InterceptorAttribute<>(new SpyInternalInterceptor<>("F", 0));
 
-
-        List<InterceptorAttribute<? extends TestEvent>> attributes = Stream.of(
+        attributes = Stream.of(
                         interceptorAttribute1, interceptorAttribute2, interceptorAttribute3,
                         interceptorAttribute4, interceptorAttribute5)
                 .collect(Collectors.toList());
+    }
 
+
+    @Test
+    public void test_parseEnabledInterceptorAttributes() {
         // 默认的启动和禁用
         Assert.assertEquals(
                 Lists.newArrayList(interceptorAttribute1, interceptorAttribute3),
@@ -46,6 +59,11 @@ public class InterceptorChainImplTest {
                         Lists.newArrayList(Rule.of("A"), Rule.of("C"), Rule.of("B"), Rule.of("-C")))
         );
 
+
+    }
+
+    @Test
+    public void test_parseEnabledInterceptorAttributes_defaultOrder() {
         // default的位置
         Assert.assertEquals(
                 Lists.newArrayList(interceptorAttribute4, interceptorAttribute5, interceptorAttribute1, interceptorAttribute3),
@@ -57,7 +75,10 @@ public class InterceptorChainImplTest {
                 interceptorChain.parseEnabledInterceptorAttributes(attributes,
                         Lists.newArrayList(Rule.of("A"), Rule.of("default"), Rule.of("C")))
         );
+    }
 
+    @Test
+    public void test_parseEnabledInterceptorAttributes_disableInterceptorInDefault() {
         // 禁用default内部的拦截器
         Assert.assertEquals(
                 Lists.newArrayList(interceptorAttribute4, interceptorAttribute1, interceptorAttribute3),
@@ -69,7 +90,10 @@ public class InterceptorChainImplTest {
                 interceptorChain.parseEnabledInterceptorAttributes(attributes,
                         Lists.newArrayList(Rule.of("default"), Rule.of("A"), Rule.of("C"), Rule.of("-D")))
         );
+    }
 
+    @Test
+    public void test_parseEnabledInterceptorAttributes_orderInDefault() {
         // default内部排序，order越小，顺序越靠前
         List<InterceptorAttribute<? extends TestEvent>> attributes2 = Stream.of(
                         interceptorAttribute1, interceptorAttribute2, interceptorAttribute3,
@@ -79,8 +103,8 @@ public class InterceptorChainImplTest {
                 Lists.newArrayList(interceptorAttribute6, interceptorAttribute4, interceptorAttribute5, interceptorAttribute1, interceptorAttribute3),
                 interceptorChain.parseEnabledInterceptorAttributes(attributes2, Lists.newArrayList(Rule.of("default"), Rule.of("A"), Rule.of("C")))
         );
-
     }
+
 
     public static class TestEvent extends Event {
 
