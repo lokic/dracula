d932753314c666d7ef26755534161b8034abd0ae
loki <kiraconan2046@gmail.com>
2023-01-21 10:42:19 +0800

    feat: 优化InterceptorComparator

--- dracula-eventbus/src/main/java/com/github/lokic/dracula/eventbus/interceptor/InterceptorComparator.java
+++ dracula-eventbus/src/main/java/com/github/lokic/dracula/eventbus/interceptor/InterceptorComparator.java
@@ -5,60 +5,21 @@ import com.google.common.collect.Ordering;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 
-public class InterceptorComparator implements Comparator<InterceptorAttribute<?>> {
-
-    /**
-     * 默认 内置拦截器 优先于 扩展拦截器
-     */
-    private static final Comparator<InterceptorAttribute<?>> COMPARATOR = Comparator
-            .<InterceptorAttribute<?>, InterceptorType>comparing(InterceptorAttribute::getType)
-            .thenComparingInt(InterceptorAttribute::getOrder);
-
-
-    private final RuleOrdering ruleOrdering;
-
-    static InterceptorComparator of(List<Rule> rules) {
-        return new InterceptorComparator(rules);
-    }
-
-    private InterceptorComparator(List<Rule> rules) {
-        this.ruleOrdering = new RuleOrdering(rules);
-    }
+public class InterceptorComparator {
 
     /**
      * 在 {@link RuleOrdering ruleOrdering} 里配置的情况下，顺序以 {@code ruleOrdering} 为准；
      * 没有{@code ruleOrdering} 里配置的情况，默认 内置拦截器 优先于 扩展拦截器，并以 {@link InterceptorAttribute#getOrder()} 排序，越小的优先级越高
-     *
-     * @param o1
-     * @param o2
-     * @return
      */
-    @Override
-    public int compare(InterceptorAttribute o1, InterceptorAttribute o2) {
-
-        if (Objects.equals(o1, o2)) {
-            return 0;
-        }
-
-        if (o1 == null) {
-            return -1;
-        }
-        if (o2 == null) {
-            return 1;
-        }
-
-        if (ruleOrdering.contains(o1) && ruleOrdering.contains(o2)) {
-            if (!Objects.equals(o1.getRule(), o2.getRule())) {
-                return ruleOrdering.compare(o1, o2);
-            }
-        }
-        return COMPARATOR.compare(o1, o2);
+    static Comparator<InterceptorAttribute<?>> of(List<Rule> rules) {
+        return Comparator.nullsFirst(new RuleOrdering(rules))
+                .thenComparing(Comparator.comparing(InterceptorAttribute::getType))
+                .thenComparingInt(InterceptorAttribute::getOrder);
     }
 
-    public static class RuleOrdering {
+    static class RuleOrdering implements Comparator<InterceptorAttribute<?>> {
 
         /**
          * rules的排序
@@ -76,8 +37,12 @@ public class InterceptorComparator implements Comparator<InterceptorAttribute<?>
             return rules.contains(o.getRule());
         }
 
-        int compare(InterceptorAttribute<?> o1, InterceptorAttribute<?> o2) {
-            return ordering.compare(o1.getRule(), o2.getRule());
+        @Override
+        public int compare(InterceptorAttribute<?> o1, InterceptorAttribute<?> o2) {
+            if (contains(o1) && contains(o2)) {
+                return ordering.compare(o1.getRule(), o2.getRule());
+            }
+            return 0;
         }
     }
 
